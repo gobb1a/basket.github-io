bbeb0c2702a54b3ed65dc6f6cd18e245be61cc93
gobb1a <Alexander.feklin675@yandex.ru>
2024-07-25 00:49:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-if ("serviceWorker" in navigator) { window.addEventListener ("load", function() {
-  navigator.serviceWorker.register("icons/sw.js") .then(    function (registration) {
+if ("serviceWorker" in navigator) {
+ window.addEventListener ("load", function() {
+  navigator.serviceWorker.register("icons/sw.js") .then(
+    function (registration) {
+
 // Registration was successful
-  console.log("ServiceWorker registration successful with scope: ", registration.scope); }, function (err) {
+  console.log("ServiceWorker registration successful with scope: ", registration.scope); },
+ function (err) {
+
 // registration failed :
-        console.log("ServiceWorker registration failed: ", err);     }) ;
+        console.log("ServiceWorker registration failed: ", err);
+     }) ;
    });
  }
