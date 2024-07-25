a5a4e6313a22dfeaf0d7f20d7f68c8d3a0a2b108
gobb1a <Alexander.feklin675@yandex.ru>
2024-07-25 11:08:44 +0300

    Update sw.js

--- icons/sw.js
+++ icons/sw.js
@@ -1,29 +1,28 @@
 const staticCacheName = 'CacheSpace1';
 
 const assetUrls = [
-  'https://gobb1a.github.io/basket.github.io//icons', 
-  'https://gobb1a.github.io/basket.github.io//style.css', 
-  'https://gobb1a.github.io/basket.github.io//background', 
-  'https://gobb1a.github.io/basket.github.io//index.html', 
-];
+  '/icons',
+  '/style.css',
+  '/background', 
+  '/index.html',
+]
+
 
 self.addEventListener('install', async event => {
-  event.waitUntil(
-    caches.open(staticCacheName)
-      .then(cache => cache.addAll(assetUrls))
-  );
-});
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assetUrls)  
+})
 
 self.addEventListener('activate', event => {
-  console.log('[SW]: activate');
-});
+  console.log('[SW]: activate')
+})
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch', event.request.url);
-  event.respondWith(cacheFirst(event.request));
-});
+  console.log('Fetch', event.request.url)
+  event.respondWith(cacheFirst(event.request))
+})
 
 async function cacheFirst(request) {
-  const cached = await cache.match(request);
-  return cached ?? await fetch(request);
+  const cached = await caches.match(request)
+  return cached ?? await fetch(request)
 }
